c25b49eb4289c8419770eb134344e9768d2910c9
danwhitlock <110103983+danwhitlock@users.noreply.github.com>
2023-06-10 15:30:57 +0100

    Complete tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,22 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  let fToC = (tempF - 32) * 5/9;
+  if (Number.isInteger(fToC)) {
+    return fToC;
+  } else {
+  return Number(fToC.toFixed(1));
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let cToF = (tempC * 9/5) + 32;
+  if (Number.isInteger(cToF)) {
+    return cToF;
+  } else {   
+    return Number(cToF.toFixed(1));  // toFixed converts the output to a string, so have to use Number() to turn it back into a number
+  }
 };
+
+
 
 // Do not edit below this line
 module.exports = {
