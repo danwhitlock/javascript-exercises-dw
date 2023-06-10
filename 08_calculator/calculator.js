d78f4be9f7bc1f747211115141d0ad870ae2f067
danwhitlock <110103983+danwhitlock@users.noreply.github.com>
2023-06-10 16:35:31 +0100

    Complete calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
+const multiply = function(...args){
+  let product = 1;
+  for (let i = 0; i < args.length; i++) {
+    product *= args[i];
+  }
+  return product;
+ };
 
+const power = function(a,b) {
+  let result = Math.pow(a,b);
+  return result;	
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num == 0) {
+    return 1;
+  } else if (num < 0) {
+    return 'error';
+  } else if (num > 0) {
+    return (num * (factorial(num -1)));
+  }
 };
 
 // Do not edit below this line
