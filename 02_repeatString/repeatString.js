74288d0fb651e52968d6aed12a2153ca176a9ca2
danwhitlock <110103983+danwhitlock@users.noreply.github.com>
2023-06-10 09:07:13 +0100

    Complete repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let resultingString = "";
+    if (num < 0) {
+        return 'ERROR';
+    } 
+    for (let i = 0; i < num; i++) {
+        resultingString += string;
+}
+return resultingString;
 };
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
