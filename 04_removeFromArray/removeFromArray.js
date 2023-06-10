8af54ba1c1241f1a84b29bc60729170b82f3e0d9
danwhitlock <110103983+danwhitlock@users.noreply.github.com>
2023-06-10 11:34:59 +0100

    Complete removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,20 @@
-const removeFromArray = function() {
-
+/*const removeFromArray = function([array], args) {
+    return array.filter(val => !args.includes(val));
 };
 
+console.log(array);
+*/
+
+const removeFromArray = function (array, ...args) {
+    const newArray = [];
+    array.forEach((item) => {
+       if (!args.includes(item)) {
+        newArray.push(item);
+      }
+    });
+     return newArray;
+  };
+
+  // console.log(removeFromArray([1,2,3], 2));
 // Do not edit below this line
 module.exports = removeFromArray;
