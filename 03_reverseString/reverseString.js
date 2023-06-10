ff5ac0fd0a6eae5bdc0fa7a8e5b3f7876f9b1d6a
danwhitlock <110103983+danwhitlock@users.noreply.github.com>
2023-06-10 10:02:46 +0100

    Complete reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    const arrayStrings = str.split("");
+    const reverseArray = arrayStrings.reverse();
+    const joinArray = reverseArray.join("");
+    return joinArray;
 };
+
+console.log(reverseString('hello there'));
 
 // Do not edit below this line
 module.exports = reverseString;
