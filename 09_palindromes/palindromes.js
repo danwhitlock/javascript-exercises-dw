34f21f6d7c056f419e3663045a30ce2394f2df11
danwhitlock <110103983+danwhitlock@users.noreply.github.com>
2023-06-11 09:02:24 +0100

    Complete palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
+// Take the starting string
+// Split it out, reverse it and join it back together
+// compare one to the other
+// add validation to ensure no special characters etc
 
+
+const palindromes = function (string) {
+    let tidiedString = string.toLowerCase().replace(/[^a-z0-9]/g,'');
+        //console.log(tidiedString);
+    let reversedString = tidiedString.split('').reverse().join('');
+        //console.log(reversedString);
+    return reversedString === tidiedString;
 };
+
+//console.log(palindromes('radar'));
+//console.log(palindromes('RaDaR'));
+//console.log(palindromes('cats'));
 
 // Do not edit below this line
 module.exports = palindromes;
