494c2e638a3500fd928c7fb7b7a10f294f625cb7
danwhitlock <110103983+danwhitlock@users.noreply.github.com>
2023-06-11 10:15:08 +0100

    Complete fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+// Set up fibonacci sequence?
+// return the value at position x?
 
+
+const fibonacci = function(position) {
+    let positionInt = Number(position);
+    if (positionInt < 0) {
+        return 'OOPS';
+    } else if (positionInt <= 1) {
+        return positionInt;
+    }
+    let result = fibonacci(positionInt - 1) + fibonacci(positionInt - 2);
+    return Number(result);
 };
 
 // Do not edit below this line
