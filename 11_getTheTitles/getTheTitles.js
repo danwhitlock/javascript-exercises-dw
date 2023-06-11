7b969a01ec6122ef1e3e105b0432ce6ecd774adb
danwhitlock <110103983+danwhitlock@users.noreply.github.com>
2023-06-11 11:52:56 +0100

    Complete getTheTitles exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
+// Use map?  Filter might only return first one?
 
+const getTheTitles = function(array) {
+    return array.map((book) => book.title);
 };
 
 // Do not edit below this line
